317af30ee525e61a430608debe482d5140795fcc
Javier <javier@tsb.lat>
2024-09-10 09:02:44 -0300

    feat: working game, only missing a fewfeatures

---
+++ player2.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("p2_move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("p2_move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	var half_height = $Sprite2D.texture.get_height() * $Sprite2D.scale.y / 2
+	var margin = 50  # Adjust this value as needed
+	position.y = clamp(position.y, half_height + margin, screen_size.y - half_height - margin)
